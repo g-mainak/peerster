8ea4f2b2629969ca1fc743d65d9d626cf187391e
Mainak Ghosh <mainak@mainak-VirtualBox.(none)>
2013-09-02 18:24:19 -0400

    Ex 2

--- main.cc
+++ main.cc
@@ -7,6 +7,20 @@
 
 #include "main.hh"
 
+MTextEdit::MTextEdit(QWidget *parent) : QTextEdit(parent) {}
+
+void MTextEdit::keyPressEvent(QKeyEvent *event)
+{
+	if (event->key() == Qt::Key_Return || event->key() == Qt::Key_Enter)
+	{
+		emit messageSent(this->toPlainText());
+		// Clear the textinput to get ready for the next input message.
+		this->clear();		
+	}
+	else
+		QTextEdit::keyPressEvent(event);
+}
+
 ChatDialog::ChatDialog()
 {
 	setWindowTitle("Peerster");
@@ -16,37 +30,26 @@ ChatDialog::ChatDialog()
 	textview = new QTextEdit(this);
 	textview->setReadOnly(true);
 
-	// Small text-entry box the user can enter messages.
-	// This widget normally expands only horizontally,
-	// leaving extra vertical space for the textview widget.
-	//
-	// You might change this into a read/write QTextEdit,
-	// so that the user can easily enter multi-line messages.
-	textline = new QLineEdit(this);
+	textinput = new MTextEdit(this);
+	textinput->setFocus(); // Set focus here.
+	connect(textinput, SIGNAL(messageSent(QString)), this, SLOT(transmitMessage(QString)));
 
 	// Lay out the widgets to appear in the main window.
 	// For Qt widget and layout concepts see:
 	// http://doc.qt.nokia.com/4.7-snapshot/widgets-and-layouts.html
 	QVBoxLayout *layout = new QVBoxLayout();
 	layout->addWidget(textview);
-	layout->addWidget(textline);
+	layout->addWidget(textinput);
 	setLayout(layout);
 
-	// Register a callback on the textline's returnPressed signal
-	// so that we can send the message entered by the user.
-	connect(textline, SIGNAL(returnPressed()),
-		this, SLOT(gotReturnPressed()));
 }
 
-void ChatDialog::gotReturnPressed()
+void ChatDialog::transmitMessage(QString message)
 {
 	// Initially, just echo the string locally.
 	// Insert some networking code here...
-	qDebug() << "FIX: send message to other peers: " << textline->text();
-	textview->append(textline->text());
-
-	// Clear the textline to get ready for the next input message.
-	textline->clear();
+	qDebug() << "FIX: send message to other peers: " << message;
+	textview->append(message);
 }
 
 NetSocket::NetSocket()
@@ -94,4 +97,3 @@ int main(int argc, char **argv)
 	// Enter the Qt main loop; everything else is event driven
 	return app.exec();
 }
-
--- main.hh
+++ main.hh
@@ -3,36 +3,49 @@
 
 #include <QDialog>
 #include <QTextEdit>
-#include <QLineEdit>
 #include <QUdpSocket>
+#include <QKeyEvent>
+
+class MTextEdit : public QTextEdit
+{
+	Q_OBJECT
+
+    public:
+    	MTextEdit(QWidget *parent);
+    	void keyPressEvent(QKeyEvent *);
+
+    signals:
+    	void messageSent(QString message);
+};
 
 class ChatDialog : public QDialog
 {
 	Q_OBJECT
 
-public:
-	ChatDialog();
+	public:
+		ChatDialog();
 
-public slots:
-	void gotReturnPressed();
+	public slots:
+		void transmitMessage(QString message);
 
-private:
-	QTextEdit *textview;
-	QLineEdit *textline;
+	private:
+		QTextEdit *textview;
+		MTextEdit *textinput;
 };
 
 class NetSocket : public QUdpSocket
 {
 	Q_OBJECT
 
-public:
-	NetSocket();
+	public:
+		NetSocket();
 
-	// Bind this socket to a Peerster-specific default port.
-	bool bind();
+		// Bind this socket to a Peerster-specific default port.
+		bool bind();
 
-private:
-	int myPortMin, myPortMax;
+	private:
+		int myPortMin, myPortMax;
 };
+
 
 #endif // PEERSTER_MAIN_HH
